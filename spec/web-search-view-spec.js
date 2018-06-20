aab1fcb9e2fbd73f6561ad037e523c342e388487
Josua Lengwenath <josua.lengwenath@shinepowered.com>
2018-06-20 11:33:25 +0200

    first commit

---
+++ spec/web-search-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import WebSearchView from '../lib/web-search-view';
+
+describe('WebSearchView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
